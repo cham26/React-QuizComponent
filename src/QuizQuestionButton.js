9f6e8912cb18c2d33549994205b9971e12d65afb
Cham <cham26@users.noreply.github.com>
2019-11-23 17:13:31 -0600

    Completed module

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -1,11 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class QuizQuestionButton extends Component {
-    render() {
-        return (
-            <li><button>{this.props.button_text}</button></li>
-        )
+    handleClick() {
+        this.props.clickHandler(this.props.button_text)
     }
+  render() {
+    return (
+      <button onClick={this.handleClick.bind(this)}>
+        {this.props.button_text}
+      </button>
+    );
+  }
 }
 
-export default QuizQuestionButton
+export default QuizQuestionButton;
